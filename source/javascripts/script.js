d2f3ab676b81ec33d131de3f280deeeb613a3438
Sacha Greif <sacha357@gmail.com>
2013-04-13 19:33:22 +0900

    added sidebar

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,21 +1,24 @@
 $(function(){
+
+  // annotator
   $('.post-content').annotator().annotator('setupPlugins', {}, {
     Filter: false
   });
-  $('body').addClass('animate');
-  $('#origin').val(document.referrer);
-  $('#landing').val(document.title);
-  $('.pullcenter img').click(function(){
-    var $this=$(this);
-    $('#lightbox').removeClass('hide').find('img').attr('src', $this.attr('src'));
-  });
-  $('#lightbox').click(function(){
-    $(this).addClass('hide');
-  });
 
+  // browser console prompt
   $('span:contains("❯")').removeClass('err').addClass('browser-prompt');
 
+  // sidebar
+  $('.toggle').click(function(event){
+    event.preventDefault();
+    $('body').toggleClass('sidebar-open')
+  })
+  $('.sidebar a').each(function(){
+    if($(this).attr('href') == window.location.pathname)
+      $(this).parent().addClass('active');
+  })
 
+  // pre
   $('pre').addClass('all-new');
 
   $('.lines-highlight').each(function(){
