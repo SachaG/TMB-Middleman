3716570c346864444980091cb9c04719c256aee6
Stephane Bellity <sbellity@gmail.com>
2013-04-19 02:56:16 +0200

    Adding initial support for Hull::Paywall

--- rack_try_static.rb
+++ rack_try_static.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
- 
 module Rack
   class TryStatic
     def initialize(app, options)
@@ -17,7 +15,7 @@ module Rack
         resp = @static.call(env.merge!({"PATH_INFO" => orig_path + path}))
         break if 404 != resp[0] && found = resp
       end
-      found or @app.call
+      found or @app.call(env)
     end
   end
 end
